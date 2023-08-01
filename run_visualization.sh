ac71e056d7809d1fbfa34559cf79d56979f4d1b2
taeo an <paxtaeo@gmail.com>
2023-08-01 23:42:59 +0000

    Add visualization script

---
+++ run_visualization.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+export CARLA_ROOT=carla_server # path to your carla root
+export CARLA_SERVER=${CARLA_ROOT}/CarlaUE4.sh
+export PYTHONPATH=$PYTHONPATH:${CARLA_ROOT}/PythonAPI
+export PYTHONPATH=$PYTHONPATH:${CARLA_ROOT}/PythonAPI/carla
+export PYTHONPATH=$PYTHONPATH:${CARLA_ROOT}/PythonAPI/carla/dist/carla-0.9.10-py3.7-linux-x86_64.egg
+export PYTHONPATH=$PYTHONPATH:$(pwd -P)/leaderboard
+export PYTHONPATH=$PYTHONPATH:$(pwd -P)/scenario_runner
+
+
+########### run KING visualization ############
+# 4 Agents
+python tools/visualize_scenarios.py \
+    --scenario_log_dir generation_results
