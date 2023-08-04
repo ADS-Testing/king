48daff6b222cfbe266c8703955ba490bca5a7ee3
paxtaeo <paxtaeo@gmail.com>
2023-08-04 02:00:13 +0000

    Upgrade carla version and Replace GlobalRoutePlannerDAO

--- run_visualization.sh
+++ run_visualization.sh
@@ -4,7 +4,7 @@ export CARLA_ROOT=carla_server # path to your carla root
 export CARLA_SERVER=${CARLA_ROOT}/CarlaUE4.sh
 export PYTHONPATH=$PYTHONPATH:${CARLA_ROOT}/PythonAPI
 export PYTHONPATH=$PYTHONPATH:${CARLA_ROOT}/PythonAPI/carla
-export PYTHONPATH=$PYTHONPATH:${CARLA_ROOT}/PythonAPI/carla/dist/carla-0.9.11-py3.7-linux-x86_64.egg
+export PYTHONPATH=$PYTHONPATH:${CARLA_ROOT}/PythonAPI/carla/dist/carla-0.9.14-py3.7-linux-x86_64.egg
 export PYTHONPATH=$PYTHONPATH:$(pwd -P)/leaderboard
 export PYTHONPATH=$PYTHONPATH:$(pwd -P)/scenario_runner
 
@@ -12,4 +12,4 @@ export PYTHONPATH=$PYTHONPATH:$(pwd -P)/scenario_runner
 ########### run KING visualization ############
 # 4 Agents
 python tools/visualize_scenarios.py \
-    --scenario_log_dir generation_results_transfuser
+    --scenario_log_dir generation_results
